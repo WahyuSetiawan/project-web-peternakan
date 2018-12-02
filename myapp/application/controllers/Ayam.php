d83f9b9292482b97b4207cc371fc105f633d3de4
wahyu setiawan <wahyu.creator911@gmail.com>
2018-12-02 23:59:54 +0700

    update

---
+++ myapp/application/controllers/Ayam.php
@@ -0,0 +1,24 @@
+<?php
+
+class AyamController extends MY_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function pembelian()
+    {
+
+    }
+
+    public function penjualan()
+    {
+
+    }
+}
