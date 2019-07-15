fe837315d507a70c320d6407ac4e73ceb950e439
Wahyu setiawan <creator@localhost.localdomain>
2019-07-15 20:29:32 +0700

    update tanggal di belum page

--- public_html/application/logs/log-2019-07-11.php
+++ public_html/application/logs/log-2019-07-11.php
@@ -1,3 +1,10 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2019-07-11 09:18:54 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-11 10:40:21 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-11 11:08:59 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-07-11 11:09:00 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-11 13:21:09 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-07-11 13:21:09 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-11 13:21:11 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-11 13:21:22 --> 404 Page Not Found: Asset/css
---
+++ public_html/application/logs/log-2019-07-15.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-07-15 08:53:45 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-07-15 08:53:46 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
+ERROR - 2019-07-15 08:53:57 --> 404 Page Not Found: Asset/css
+ERROR - 2019-07-15 09:22:55 --> 404 Page Not Found: Faviconico/index
+ERROR - 2019-07-15 09:25:05 --> Severity: Notice --> Undefined variable: current_date_target /usr/share/httpd/www/application/cache/views/d3baa3a716411bf7a60e958f92a8792f 154
+ERROR - 2019-07-15 09:25:05 --> Severity: Notice --> Undefined variable: current_date_target /usr/share/httpd/www/application/cache/views/d3baa3a716411bf7a60e958f92a8792f 154
