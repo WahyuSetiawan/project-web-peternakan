94e05454af67c67f42ba91d1923ea1c3e1755904
kijang <kijang@localhost.localdomain>
2019-08-03 20:35:19 +0700

    remote application/logs

--- application/logs/log-2019-07-07.php
+++
@@ -1,3 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-07-07 22:10:10 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
--- application/logs/log-2019-07-30.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2019-07-30 00:20:46 --> 404 Page Not Found: Faviconico/index
-ERROR - 2019-07-30 00:20:47 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
-ERROR - 2019-07-30 01:22:01 --> 404 Page Not Found: Faviconico/index
-ERROR - 2019-07-30 01:22:02 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
-ERROR - 2019-07-30 01:22:03 --> Severity: error --> Exception: Unable to locate the model you have specified: DetailPersediaanModel /usr/share/httpd/www/system/core/Loader.php 344
-ERROR - 2019-07-30 01:28:50 --> 404 Page Not Found: Faviconico/index
